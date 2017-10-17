cb122017b96ff72480624e93f648f4e3ec1622d6
Sylvance <kerandisylvance@gmail.com>
2017-10-17 14:36:03 +0300

    Add a configuration file

---
+++ config.py
@@ -0,0 +1,6 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'this-is-top-secret'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
