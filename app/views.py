66f2830f2210e57c305f9dcb1598d6f0f939e270
Sylvance <kerandisylvance@gmail.com>
2017-10-18 09:36:21 +0300

    Change Views file to reference host and port

--- app/views.py
+++ app/views.py
@@ -5,55 +5,55 @@ from .forms import SigninForm
 @app.route('/')
 @app.route('/home')
 def index():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('home.html',
                            title='Home',
                            user=user)
 @app.route('/addcategory')
 def addcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('addcategory.html',
                            title='addcategory',
                            user=user)
 
 @app.route('/addrecipe')
 def addrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('addrecipe.html',
                            title='addrecipe',
                            user=user)
 
 @app.route('/category')
 def category():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('category.html',
                            title='category',
                            user=user)
 
 @app.route('/editcategory')
 def editcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('editcategory.html',
                            title='editcategory',
                            user=user)
 
 @app.route('/editrecipe')
 def editrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('editrecipe.html',
                            title='editrecipe',
                            user=user)
 
 @app.route('/profile')
 def profile():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('profile.html',
                            title='profile',
                            user=user)
 
 @app.route('/recipe')
 def recipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('recipe.html',
                            title='recipe',
                            user=user)
@@ -72,21 +72,27 @@ def signin():
 
 @app.route('/signup')
 def signup():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('signup.html',
                            title='signup',
                            user=user)
 
 @app.route('/viewcategory')
 def viewcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('viewcategory.html',
                            title='viewcategory',
                            user=user)
 
 @app.route('/viewrecipe')
 def viewrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}  
+    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
     return render_template('viewrecipe.html',
                            title='viewrecipe',
                            user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True,
+            host="0.0.0.0",
+            port="8888")
+    
