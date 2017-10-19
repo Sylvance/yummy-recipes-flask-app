f066751e743418cda1f78c44964540ffe845e267
Sylvance <kerandisylvance@gmail.com>
2017-10-19 15:11:07 +0300

    [Feature #152100180] Make Flask authentication logic

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+""" Views file for the flask app"""
 from flask import render_template, flash, redirect
 from app import app
 from .forms import (SigninForm,
@@ -7,82 +8,114 @@ from .forms import (SigninForm,
                     EditcategoryForm,
                     EditrecipeForm)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('home.html',
                            title='Home',
                            user=user)
+
+
 @app.route('/addcategory', methods=['GET', 'POST'])
 def addcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     form = AddcategoryForm()
     if form.validate_on_submit():
+
         return redirect('/profile')
     return render_template('addcategory.html',
                            title='addcategory',
                            user=user,
                            form=form)
 
+
 @app.route('/addrecipe', methods=['GET', 'POST'])
 def addrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     form = AddrecipeForm()
     if form.validate_on_submit():
+
         return redirect('/profile')
     return render_template('addrecipe.html',
                            title='addrecipe',
                            user=user,
                            form=form)
 
+
 @app.route('/category')
 def category():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('category.html',
                            title='category',
                            user=user)
 
+
 @app.route('/editcategory', methods=['GET', 'POST'])
 def editcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     form = EditcategoryForm()
     if form.validate_on_submit():
+
         return redirect('/profile')
     return render_template('editcategory.html',
                            title='editcategory',
                            user=user,
                            form=form)
 
+
 @app.route('/editrecipe', methods=['GET', 'POST'])
 def editrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     form = EditrecipeForm()
     if form.validate_on_submit():
+
         return redirect('/profile')
     return render_template('editrecipe.html',
                            title='editrecipe',
                            user=user,
                            form=form)
 
+
 @app.route('/profile')
 def profile():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('profile.html',
                            title='profile',
                            user=user)
 
+
 @app.route('/recipe')
 def recipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('recipe.html',
                            title='recipe',
                            user=user)
 
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
+    """ Place a Docstring here """
     form = SigninForm()
     if form.validate_on_submit():
+
         flash('Signin requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
         return redirect('/profile')
@@ -91,25 +124,34 @@ def signin():
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """ Place a Docstring here """
     form = SignupForm()
     if form.validate_on_submit():
+
         return redirect('/profile')
     return render_template('signup.html',
                            title='signup',
                            form=form)
 
+
 @app.route('/viewcategory')
 def viewcategory():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('viewcategory.html',
                            title='viewcategory',
                            user=user)
 
+
 @app.route('/viewrecipe')
 def viewrecipe():
-    user = {'nickname': 'Sylvance', 'job' : 'Carpernter', 'categoriesno' : 8, 'recipesno' : 23}
+    """ Place a Docstring here """
+    user = {'nickname': 'Sylvance', 'job': 'Carpernter',
+            'categoriesno': 8, 'recipesno': 23}
     return render_template('viewrecipe.html',
                            title='viewrecipe',
                            user=user)
@@ -118,4 +160,3 @@ if __name__ == '__main__':
     app.run(debug=True,
             host="0.0.0.0",
             port="8888")
-    
